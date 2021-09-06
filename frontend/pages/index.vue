2ebdf74d3037ea0908d93426b513e8348cecb701
Draeston <69845877+Draeston@users.noreply.github.com>
2021-09-06 21:54:10 +0300

    nav-bar template

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <v-app-bar>
-    <v-img alt="logo"></v-img>
+  <v-app-bar class="main-app-bar">
+    <v-tab>Logo</v-tab>
+    <v-tab>John Smith</v-tab>
+    <v-spacer></v-spacer>
+    <v-tab>Profile</v-tab>
+    <v-tab>Logout</v-tab>
   </v-app-bar>
 </template>
+
+<style>
+.main-app-bar {
+  background: rgb(13,156,34);
+  background: linear-gradient(90deg, rgba(13,156,34,0.44947485830269607) 0%, rgba(72,194,118,0.37664572665003504) 66%);
+}
+</style>
